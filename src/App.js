9a41b869cae9c405d7cfcfbf4874713cdd574c96
ssy0228 <alchemist0228@gmail.com>
2022-04-21 11:46:47 +0900

    BrowserRouter>HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.scss";
 import Friends from "./routes/Friends";
 import More from "./routes/More";
